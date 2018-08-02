e3dd92995f090c44fd1c274dc669499bc2a1bfb2
Michael Korbakov <rmihael@gmail.com>
2018-08-03 00:07:37 +0300

    Implemented lambda

--- lambda.py
+++ lambda.py
@@ -1,23 +1,12 @@
+import os
+
+from purl import URL
+
 from monitor.models import FinhubMonitor
 
 
 def handler(event, context):
-    """Sample pure Lambda function
-
-    Arguments:
-        event LambdaEvent -- Lambda Event received from Invoke API
-        context LambdaContext -- Lambda Context runtime methods and attributes
-
-    Returns:
-        dict -- {'statusCode': int, 'body': dict}
-    """
-
-    ip = requests.get('http://checkip.amazonaws.com/')
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            'message': 'hello world',
-            'location': ip.text.replace('\n', ''),
-        })
-    }
+    monitor = FinhubMonitor(URL("https://my.finhub.ua/portal-rs/api"))
+    token = monitor.login(os.environ['EMAIL'], os.environ['PASSWORD'])
+    loans = monitor.get_loan_requests(token)
+    return str(loans)
