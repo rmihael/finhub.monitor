16c0dfe13ff2d4bf98ea1d52953b462df13200b4
Michael Korbakov <rmihael@gmail.com>
2018-08-04 23:01:02 +0300

    Fix for get_interesting_loans

--- monitor/models.py
+++ monitor/models.py
@@ -70,12 +70,11 @@ class FinHubMonitor:
 
     def get_interesting_loans(self, max_risk_level: int) -> List[LoanRequest]:
         loans = self._api.get_loan_requests()
-        print(loans)
         log.info("Got %s loans from API" % len(loans))
         new_ids = self._known_loans.filter_out_known_ids([l.loan_id for l in loans])
         new_loans = [l for l in loans if l.loan_id in new_ids]
         log.info("Got %s new loans" % len(new_loans))
-        interesting_loans = [l for l in new_loans if l.risk_level <= max_risk_level and l.rest > 0]
+        interesting_loans = [l for l in new_loans if l.risk_level_group <= max_risk_level and l.rest > 0]
         log.info("Got %s interesting loans" % len(interesting_loans))
         self._known_loans.memorize_loans(new_ids, datetime.utcnow())
         return interesting_loans
